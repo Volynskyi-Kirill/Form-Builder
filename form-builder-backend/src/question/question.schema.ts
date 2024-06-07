b201a840fbe93693a1f875a39fa7b18d57819c00
Volynskyi-Kirill <kirill108bol@gmail.com>
2024-06-07 18:40:35 +0200

    feat: add conection to DB

---
+++ form-builder-backend/src/question/question.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { QUESTION_TYPE } from 'src/shared/enums';
+
+export type QuestionDocument = HydratedDocument<Question>;
+
+@Schema()
+export class Question {
+  @Prop({ enum: QUESTION_TYPE, required: true })
+  type: QUESTION_TYPE;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  text: string;
+}
+
+export const QuestionSchema = SchemaFactory.createForClass(Question);
