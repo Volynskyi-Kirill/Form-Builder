09913a2587174acda7d4eb48e9ebec51bd37077c
Volynskyi-Kirill <kirill108bol@gmail.com>
2024-06-14 15:52:39 +0200

    config: add cors

--- form-builder-backend/src/main.ts
+++ form-builder-backend/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CorsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(CorsConfig);
 
   await app.listen(PORT);
 }
